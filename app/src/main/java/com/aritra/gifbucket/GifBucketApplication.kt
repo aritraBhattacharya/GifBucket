6ff3151d910a3b05242cc95978f7944ada94f7f2
Aritra Bhattacharya <aritracrab@gmail.com>
2022-10-21 03:01:14 +0530

    added api key in gradle.properties

---
+++ app/src/main/java/com/aritra/gifbucket/GifBucketApplication.kt
@@ -0,0 +1,10 @@
+package com.aritra.gifbucket
+
+import android.app.Application
+
+class GifBucketApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // initiate logging here according to build type
+    }
+}
