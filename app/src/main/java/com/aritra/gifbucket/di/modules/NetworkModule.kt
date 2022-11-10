875471cf7fc137fe7ef423dc37af6e50e0df26e4
Aritra Bhattacharya <aritracrab@gmail.com>
2022-11-10 22:54:40 +0530

    added okHttp client options and interceptors

--- app/src/main/java/com/aritra/gifbucket/di/modules/NetworkModule.kt
+++ app/src/main/java/com/aritra/gifbucket/di/modules/NetworkModule.kt
@@ -3,7 +3,10 @@ package com.aritra.gifbucket.di.modules
 import com.aritra.gifbucket.data.remote.network.GifAPI
 import dagger.Module
 import dagger.Provides
+import kotlinx.coroutines.runBlocking
 import okhttp3.OkHttpClient
+import okhttp3.Route
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -29,7 +32,27 @@ class NetworkModule {
     @Singleton
     fun provideOkHttpClient(): OkHttpClient {
         val clientBuilder = OkHttpClient.Builder()
+
+//        val httpLoggingInterceptor = HttpLoggingInterceptor()
+//        if (BuildConfig.DEBUG) {
+//            logging.level = HttpLoggingInterceptor.Level.HEADERS
+//            logging.level = HttpLoggingInterceptor.Level.BODY
+//        }
+
+//        clientBuilder.cache() --> cache file
+//        client.addInterceptor(HeaderInterceptor()) --> for adding header values
+//        clientBuilder.addInterceptor(httpLoggingInterceptor)  --> add something like logging interceptor : HttpLoggingInterceptor
+//        clientBuilder.addNetworkInterceptor() --> add the online interceptor for cache here
+//        clientBuilder.addInterceptor() --> add offline interceptor for cache here
+//        clientBuilder.authenticator() --> for access and refresh token
+//        clientBuilder.certificatePinner() --> for ssl certificate pinning
+//        clientBuilder.addInterceptor(GzipRequestInterceptor()) --> for data compression over internet
+//        clientBuilder.connectTimeout()
+//        clientBuilder.readTimeout(60, TimeUnit.SECOND)
+//        clientBuilder.writeTimeout(60, TimeUnit.SECOND)
+//        clientBuilder.connectionPool(60, TimeUnit.SECOND)
         return clientBuilder.build()
+
     }
 
     @Provides
